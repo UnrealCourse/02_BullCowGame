ea4da96d6f0b78beb4e39c42bcae8e376e10ae31
Ben Tristem <github@bentristem.com>
2016-01-05 19:29:30 +0000

    BC12 booleans and comparisons

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 void PrintIntro();
 void PlayGame();
 string GetGuess();
-
+bool AskToPlayAgain();
 
 // the entry point for our application
 int main()
 {
 	PrintIntro();
 	PlayGame();
+	AskToPlayAgain();
 	return 0; // exit the application
 }
 
@@ -48,4 +49,12 @@ string GetGuess()
 	string Guess = "";
 	getline(cin, Guess);
 	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again? ";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
