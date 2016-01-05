c369e6bb2d56a98304199ebe7168a6235e6b4730
Ben Tristem <github@bentristem.com>
2016-01-05 12:34:39 +0000

    BC11 Clarity is Worth Fighting For

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -4,23 +4,18 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
+
 
 // the entry point for our application
 int main()
 {
 	PrintIntro();
-
-	// loop for the number of turns asking for guesses
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
-	{
-		GetGuessAndPrintBack();
-		cout << endl;
-	}
-
-	return 0;
+	PlayGame();
+	return 0; // exit the application
 }
+
 
 // introduce the game
 void PrintIntro()
@@ -34,14 +29,23 @@ void PrintIntro()
 }
 
 
-string GetGuessAndPrintBack()
+void PlayGame()
+{
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+
+string GetGuess()
 {
 	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-
-	// print the guess back
-	cout << "Your guess was: " << Guess << endl;
 	return Guess;
 }
