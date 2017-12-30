193e922cac522b334a109dd07eb97f0df79cae79
Bogdan MATA <bogdanmata@gmail.com>
2017-12-30 19:20:21 +0100

    Some proposals

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -26,28 +26,25 @@ void FBullCowGame::Reset()
 
 	MyCurrentTry = 1;
 	bGameIsWon = false;
-	return;
 }
 
 
 EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const
 {
+	// we are returning ;)
 	if (!IsIsogram(Guess)) // if the guess isn't an isogram
 	{
 		return EGuessStatus::Not_Isogram;
 	}
-	else if (!IsLowercase(Guess)) // if the guess isn't all lowercase
+	if (!IsLowercase(Guess)) // if the guess isn't all lowercase
 	{
 		return EGuessStatus::Not_Lowercase;
 	}
-	else if (Guess.length() != GetHiddenWordLength()) // if the guess length is wrong
+	if (Guess.length() != GetHiddenWordLength()) // if the guess length is wrong
 	{
 		return EGuessStatus::Wrong_Length;
 	}
-	else
-	{
-		return EGuessStatus::OK;
-	}
+	return EGuessStatus::OK;
 }
 
 
@@ -59,26 +56,21 @@ FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess)
 	int32 WordLength = MyHiddenWord.length(); // assuming same length as guess
 
 	// loop through all letters in the hidden word
-	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++) {
+	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++)
+	{
 		// compare letters against the guess
-		for (int32 GChar = 0; GChar < WordLength; GChar++) {
+		for (int32 GChar = 0; GChar < WordLength; GChar++)
+		{
 			// if they match then
-			if (Guess[GChar] == MyHiddenWord[MHWChar]) {
-				if (MHWChar == GChar) { // if they're in the same place
-					BullCowCount.Bulls++; // incriment bulls
-				}
-				else {
-					BullCowCount.Cows++; // must be a cow
-				}
+			if (Guess[GChar] == MyHiddenWord[MHWChar])
+			{
+				(MHWChar == GChar) ? BullCowCount.Bulls++ : BullCowCount.Cows++;
 			}
 		}
 	}
-	if (BullCowCount.Bulls == WordLength) {
-		bGameIsWon = true;
-	}
-	else
+	if (BullCowCount.Bulls == WordLength)
 	{
-		bGameIsWon = false;
+		bGameIsWon = true;
 	}
 	return BullCowCount;
 }
@@ -92,11 +84,11 @@ bool FBullCowGame::IsIsogram(FString Word) const
 	for (auto Letter : Word) 	// for all letters of the word
 	{
 		Letter = tolower(Letter); // handle mixed case
-		if (LetterSeen[Letter]) {// if the letter is in the map
+		if (LetterSeen[Letter])
+		{// if the letter is in the map
 			return false; // we do NOT have an isogram
-		} else { 
-			LetterSeen[Letter] = true;// add the letter to the map
-		}	
+		}
+		LetterSeen[Letter] = true;// add the letter to the map
 	}
 
 	return true; // for example in cases where /0 is entered
